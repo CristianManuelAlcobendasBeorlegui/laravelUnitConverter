6f6d19c6d90ac6f3812a60ee4e93eb40961cb438
Cristian Manuel Alcobendas Beorlegui <cristian.alcobendas@insbaixcamp.cat>
2024-02-29 19:04:47 +0100

    Se añaden pruebas unitarias para la API conversor de longitud

--- tests/Unit/ApiTest.php
+++ tests/Unit/ApiTest.php
@@ -10,7 +10,18 @@ use App\Http\Controllers\ConvertWeightController;
 use PHPUnit\Framework\TestCase;
 
 class ApiTest extends TestCase {
-
+    /** 
+     * Tests for 'ConvertLengthController' class.
+     * */
+    public function test_convert_length_controller() {
+        $convertLengthController = new ConvertLengthController();
+        $response = $convertLengthController->__invoke('adds', '100');
+        $this->assertEquals(400, $response['code']);
+        $this->assertEquals('ERROR: Value (adds) is not a number.', $response['content']['msg']);
+        $response = $convertLengthController->__invoke('1', 'meters');
+        $this->assertEquals('FEETS', $response['content']['valueConvertedTo']);
+        $this->assertEquals(3.28084, $response['content']['valueConverted']);
+    }
 }
 
 ?>
