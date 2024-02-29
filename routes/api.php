7cbcbfba60c7cf4bb3b2affdf8465a690559bf39
Cristian Manuel Alcobendas Beorlegui <cristian.alcobendas@insbaixcamp.cat>
2024-02-29 19:07:10 +0100

    Se añade ruta API del conversor de velocidad

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ConvertLengthController;
+use App\Http\Controllers\ConvertSpeedController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,3 +21,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('/converter/length/{value}/{unit}', ConvertLengthController::class);
+Route::get('/converter/speed/{value}/{unit}', ConvertSpeedController::class);
