cf9e1d0a4c40c33e78d43303b0f584de695b77ca
Cristian Manuel Alcobendas Beorlegui <cristian.alcobendas@insbaixcamp.cat>
2024-02-29 19:03:50 +0100

    Creacion fichero para pruebas unitarias de la API

---
+++ tests/Unit/ApiTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Http\Controllers\ConvertLengthController;
+use App\Http\Controllers\ConvertSpeedController;
+use App\Http\Controllers\ConvertTemperatureController;
+use App\Http\Controllers\ConvertVolumeController;
+use App\Http\Controllers\ConvertWeightController;
+use PHPUnit\Framework\TestCase;
+
+class ApiTest extends TestCase {
+
+}
+
+?>
